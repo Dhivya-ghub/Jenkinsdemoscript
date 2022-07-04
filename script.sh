693aef9993bb904dda90d2b1c1ea348d6467eef9
Dhivya-ghub <90086855+Dhivya-ghub@users.noreply.github.com>
2022-07-04 19:25:13 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+URL_CURL_CHECK="localhost:5008/"
+VAR_A=$(curl -Is --head "$URL_CURL_CHECK" | grep -o "HTTP/1.1 200 OK")
+VAR_B="HTTP/1.1 200 OK"
+echo
+if [ "$VAR_A" != "$VAR_B" ]; then
+    echo "Not equal ..."
+else [ "$VAR_A" = "$VAR_B" ]
+    echo "It's a match"
+fi
+echo
+echo "Validate VARs: "
+echo "$VAR_A"
+echo "$VAR_B"
