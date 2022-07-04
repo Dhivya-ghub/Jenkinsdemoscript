0308992a7842f33e5ec6b26722dbe601911c65ab
Dhivya-ghub <90086855+Dhivya-ghub@users.noreply.github.com>
2022-07-04 20:15:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 URL_CURL_CHECK="localhost:5008/"
-VAR_A=$(curl -Is --head "$URL_CURL_CHECK" | grep -o "HTTP/1.1 200 OK")
+VAR_A=$(curl "$URL_CURL_CHECK" | grep -o "HTTP/1.1 200 OK")
 VAR_B="HTTP/1.1 200 OK"
 echo
 if [ "$VAR_A" != "$VAR_B" ]; then
