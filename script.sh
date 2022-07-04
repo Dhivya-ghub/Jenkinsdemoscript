dc9592343322bafd6d4e04d462ff8a9585e4629d
Dhivya-ghub <90086855+Dhivya-ghub@users.noreply.github.com>
2022-07-04 20:30:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,8 @@
 URL_CURL_CHECK="localhost:5008/"
-VAR_A=$(curl "$URL_CURL_CHECK" | grep -o "200 OK")
+VAR_A=$(curl "$URL_CURL_CHECK" | grep "200 OK")
 VAR_B="200 OK"
 echo
 if [ "$VAR_A" != "$VAR_B" ]; then
     echo "equal ..."
 else [ "$VAR_A" = "$VAR_B" ]
     echo "It's not a match"
-fi
-echo
-echo "Validate VARs: "
-echo "$VAR_A"
-echo "$VAR_B"
