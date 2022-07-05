5db43b2c830ff1e0cf7a25dad79d76e16ec28794
Dhivya-ghub <90086855+Dhivya-ghub@users.noreply.github.com>
2022-07-05 12:55:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,8 +4,10 @@ VAR_B="200 OK"
 echo
 if [ "$VAR_A" != "$VAR_B" ]; then
     echo "equal ..."
+    exit 0
 else [ "$VAR_A" = "$VAR_B" ]
     echo "It's not a match"
+    exit 1
 fi
 echo
 echo "Validate VARs: "
