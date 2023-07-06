34dd9b01da1fb4c1d9c20fa485d1ae4f37f833da
Dhivya-ghub <90086855+Dhivya-ghub@users.noreply.github.com>
2023-07-06 14:56:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+FROM python:2.7-slim
+ENV http_proxy http://host.docker.internal:3128
+ENV https_proxy http://host.docker.internal:3128
+WORKDIR /app
+COPY . .
+RUN pip install -r requirements.txt
+EXPOSE 5000
+CMD ["python", "app.py"]
